dae629c275244ebdc8c29bfaa92e5ea00a261ca1
aristofanis1 <67842756+aristofanis1@users.noreply.github.com>
2020-07-05 20:50:18 +0300

    Add files via upload

--- SolarSystem/Main.gd
+++ SolarSystem/Main.gd
@@ -12,9 +12,23 @@ func _ready():
 	pass
 
 
+func apply_gravitational_force(from, to, delta, G_overrite=G) :
+	var dist = from.global_position.distance_squared_to(to.global_position)
+	to.apply_central_impulse((from.global_position-to.global_position).normalized()*G_overrite*delta*from.mass*to.mass/dist)
+	
 func _process(delta):
 	$EarthOrbit.add_point($Earth.global_position)
+	
+	if $EarthOrbit.get_point_count() > 2000 :
+		$EarthOrbit.remove_point(0) 
+		
+	$MoonOrbit.add_point($Moon.global_position)
+	
+	if $MoonOrbit.get_point_count() > 2000 :
+		$MoonOrbit.remove_point(0) 
 
 func _physics_process(delta):
-	var dist = $Sun.position.distance_squared_to($Earth.position)
-	$Earth.apply_central_impulse(($Sun.position-$Earth.position).normalized()*G*delta*$Sun.mass*$Earth.mass/dist)
+	apply_gravitational_force($Sun, $Earth, delta)
+	apply_gravitational_force($Sun, $Moon, delta)
+	apply_gravitational_force($Earth, $Moon, delta, G*4)
+	apply_gravitational_force($Moon, $Earth, delta, G*4)	
